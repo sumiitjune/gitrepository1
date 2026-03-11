b6b8be516fee56162ff9ed0d4c66b15fe28ddccd
sumitkr <sumitjunegmail.com>
2026-03-11 08:27:31 +0000

    second commit

--- test2.py
+++ test2.py
@@ -1,1 +1,4 @@
-print("hello sumit this side  this is test2");
+print("Hello from my Git practice project!")
+
+name = input("Enter your name: ")
+print("Welcome", name)print("hello sumit this side  this is test2");
