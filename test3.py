a58f19f0cb317255becdeaad60230e6afcc6154b
sumitkr <sumitjunegmail.com>
2026-03-11 08:32:40 +0000

    code added in test2

--- test3.py
+++ test3.py
@@ -1,1 +1,7 @@
-print("hii sumit this side this test3");
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)print("hii sumit this side this test3");
